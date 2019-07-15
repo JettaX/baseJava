3e336d204689d8702110c5818c57c1aabaad0cbd
Vadim <vadim91111@gmail.com>
2019-07-15 11:54:55 +0400

    new homeWork HW1

---
+++ src/lesson_1/MainArray.java
@@ -0,0 +1,13 @@
+package lesson_1;
+
+public class MainArray {
+    public static void main(String[] args) {
+        ArrayStorage storage1 = new ArrayStorage();
+        ArrayStorage storage2 = new ArrayStorage();
+        ArrayStorage storage3 = new ArrayStorage();
+
+        storage1.save("r55");
+        storage2.save("r78");
+        storage3.save("r32");
+    }
+}
