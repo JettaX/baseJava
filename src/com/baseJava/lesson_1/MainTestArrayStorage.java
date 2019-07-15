2bf3ab897e876bdeda4f55a08868203a419de7f2
Vadim <vadim91111@gmail.com>
2019-07-15 12:02:40 +0400

    new dirictory

---
+++ src/com/baseJava/lesson_1/MainTestArrayStorage.java
@@ -0,0 +1,19 @@
+package com.baseJava.lesson_1;
+
+import java.util.Arrays;
+
+public class MainTestArrayStorage {
+    public static void main(String[] args) {
+        ArrayStorage storage = new ArrayStorage();
+        storage.save("r1");
+        storage.save("r2");
+        storage.save("r3");
+        storage.save("r4");
+        System.out.println(storage.get(3));
+        System.out.println(storage.size());
+        storage.delete(1);
+        System.out.println(Arrays.toString(storage.getAll()));
+        storage.clear();
+        System.out.println(Arrays.toString(storage.getAll()));
+    }
+}
